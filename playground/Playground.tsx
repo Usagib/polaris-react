2272816d6d1de13bf0fefd983b4b23a9f05481e3
Patrick Racicot <patrick.racicot@shopify.com>
2021-08-02 09:23:19 -0400

    broken button

--- playground/Playground.tsx
+++ playground/Playground.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
 
-import {Page} from '../src';
+import {
+  Button,
+  Card,
+  Link,
+  Page,
+  Stack,
+  ThemeProvider,
+  Thumbnail,
+} from '../src';
 
 export function Playground() {
   return (
     <Page title="Playground">
-      {/* Add the code you want to test in here */}
+      <ThemeProvider
+        theme={{
+          colorScheme: 'dark',
+          colors: {
+            surface: '#020c1d',
+            secondary: '#103560',
+            interactive: '#009973',
+          },
+        }}
+      >
+        <Card title="Order details">
+          <Card.Section title="unfulfilled">
+            <Stack alignment="center">
+              <Thumbnail
+                alt=""
+                source="https://burst.shopifycdn.com/photos/lightning-hard-enamel-lapel-pin.jpg"
+              />
+              <Stack.Item fill>
+                <p>
+                  <Link removeUnderline>Enamel pin</Link>
+                </p>
+              </Stack.Item>
+              <Stack.Item>
+                <p>$9.99</p>
+              </Stack.Item>
+            </Stack>
+          </Card.Section>
+          <Card.Section>
+            <Stack alignment="center">
+              <Stack.Item fill>Buy postage and ship 2 items</Stack.Item>
+              <Stack.Item>
+                <Button outline>Continue</Button>
+              </Stack.Item>
+            </Stack>
+          </Card.Section>
+        </Card>
+      </ThemeProvider>
     </Page>
   );
 }
